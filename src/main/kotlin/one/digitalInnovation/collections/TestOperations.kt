d41b5e09f0beb9e34747c04950035c5730291c7c
Willian Brandão <willian.ramos@aluno.cefet-rj.br>
2021-04-08 13:34:57 -0300

    implement count, find, any

--- src/main/kotlin/one/digitalInnovation/collections/TestOperations.kt
+++ src/main/kotlin/one/digitalInnovation/collections/TestOperations.kt
@@ -1,7 +1,7 @@
 package one.digitalInnovation.collections
 
 fun main(){
-    val salaries = doubleArrayOf(100.0, 906.0, 235.0)
+    val salaries = doubleArrayOf(100.0, 906.0, 235.0, 890.0, 456.0, 543.0, 267.0)
 
     for(salary in salaries){
         println(salary)
@@ -11,8 +11,16 @@ fun main(){
     println("Lowest salary:  ${salaries.min()}")
     println("Average salary:  ${salaries.average()}")
     println("*************************************")
-    val highSalaryThan250 = salaries.filter { it > 250 }
+    val highSalaryThan250 = salaries.filter { it > 600 }
     highSalaryThan250.forEach{
         println(it)
     }
+    println("*************************************")
+    println(salaries.count{it in 900.0..1000.0})
+
+    println("*************************************")
+    println(salaries.find{it == 675.0})
+
+    println("*************************************")
+    println(salaries.any{it == 675.0})
 }
