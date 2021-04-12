02694a091ce75ebff956a36121c133dadeb99598
Willian Brandão <willian.ramos@aluno.cefet-rj.br>
2021-04-12 11:53:26 -0300

    create repository class with mutable map and create file to test this map

---
+++ src/main/kotlin/one/digitalInnovation/collections/TestMutableMap.kt
@@ -0,0 +1,28 @@
+package one.digitalInnovation.collections
+
+fun main(){
+
+    val joaquim = Employee( "Joaquim", 1650.00, "CLT")
+    val raquel = Employee( "Raquel", 1400.00, "PJ")
+    val raul = Employee( "Raul", 1100.00, "CLT")
+    val luisa = Employee( "Luisa", 1970.00, "PJ")
+
+    val repository = Repository<Employee>()
+
+    repository.create(joaquim.name, joaquim)
+    repository.create(luisa.name, luisa)
+    repository.create(raul.name, raul)
+    repository.create(raquel.name, raquel)
+
+    println(repository.findById(joaquim.name))
+
+    println("===============================")
+    repository.findAll().forEach { println(it) }
+
+
+    println("===============================")
+    repository.remove(joaquim.name)
+    repository.findAll().forEach { println(it) }
+
+
+}
